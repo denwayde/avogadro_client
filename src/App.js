cf805b8da1c966608a7ee0fb4c5fc6c2332b619c
denwayde <denwayde09@gmail.com>
2025-04-04 15:20:06 +0500

    lgff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { useEffect } from 'react';
 import './App.css';
+const tg = window.Telegram.WebApp
 
 function App() {
+  const Close = ()=>{
+    tg.close()
+  }
+  useEffect(()=>{
+    tg.ready()
+  }, [])
   return (
     <div className="App">
       <h1>Привет мир</h1>
+      <butto onClick = {Close}>Close</butto>
     </div>
   );
 }
