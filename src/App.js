ee8329804f920ac3b2ad10a306b2cb38cf69b777
denwayde <denwayde09@gmail.com>
2025-04-04 15:23:19 +0500

    цц

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1>Привет мир</h1>
-      <butto onClick = {Close}>Close</butto>
+      <button onClick = {Close}>Close</button>
     </div>
   );
 }
