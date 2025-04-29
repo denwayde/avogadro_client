2f2b02309e7da06974283fa70ee8b3039830e167
denwayde <denwayde09@gmail.com>
2025-04-29 13:33:30 +0500

    new icon

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import icon from './img/icon.png'
-
+//import icon from './img/icon.png'
+import icon1 from './img/atom-icon.png'
 function Header() {
     return (
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid d-flex justify-content-center">
                 
-                <span className='navbar-brand'>
-                    <img src={icon} alt="Logo" width="95" height="55" className="d-inline-block align-text-top"/>
+                <span className='navbar-brand my-brand'>
+                    <img src={icon1} alt="Logo" className="d-inline-block align-text-top"/>
                 </span>
 
             </div>
